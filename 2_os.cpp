dc5e9ce42fdbdfca45fc9c0dfb33094bd963306d
Vizir Vadim <vizir.vadim@mail.ru>
2020-02-07 22:36:31 +0700

    init git

---
+++ 2_os.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+
+int main()
+{	
+	int daylight;
+	long long timezone ;
+	const char* tzname[2];
+	time_t t;
+	struct tm* sp;
+	t = time(nullptr);
+	sp = localtime(&t);
+	_putenv("TZ=PST");
+	_tzset();
+	printf("%d %d %d\n", sp->tm_hour, timezone, daylight);
+	return 0;
+}
